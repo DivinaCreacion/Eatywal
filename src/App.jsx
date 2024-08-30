82ea75685475162f5860d9ccf528a1c5f2c740b6
DivinaCreacion <jcortesm2@unicartagena.edu.co>
2024-08-29 20:23:13 -0500

    Adelantos del Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-// Supports weights 100-900
+
+import './index.css'
+import Header from './Components/Header';
 import '@fontsource-variable/saira';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-normal text-blue-500">
-        Hello world!
-      </h1>
+      <Header />
     </>
   )
 }
